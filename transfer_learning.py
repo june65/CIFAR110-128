f4983b9fe09d9a772a453031da55ed66dfd0ca0c
june0605 <june0100726@gmail.com>
2022-09-15 15:25:48 +0900

    Add CIFAR_v2

--- transfer_learning.py
+++ transfer_learning.py
@@ -90,7 +90,7 @@ val_gen  = train_datagen.flow_from_directory('./data/train',
                                                  target_size = (150, 150),
                                                  batch_size = 32,
                                                  class_mode = 'categorical',subset='validation')
-
+'''
 # Initialising the CNN
 cnn = tf.keras.models.Sequential()
 
@@ -122,3 +122,240 @@ cnn.summary()
 cnn.fit(x = train_gen, validation_data = val_gen, epochs = 10)
 
 cnn.save('./CIFAR_v2')
+'''
+def create_gen():
+    # 생성기 및 데이터 증강으로 이미지 로드
+    train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
+        validation_split=0.1
+    )
+
+    test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input
+    )
+
+    train_images = train_generator.flow_from_dataframe(
+        dataframe=train_df,
+        x_col='Filepath', # 파일위치 열이름
+        y_col='Label', # 클래스 열이름
+        target_size=(224, 224), # 이미지 사이즈
+        color_mode='rgb', # 이미지 채널수
+        class_mode='categorical', # Y값(Label값)
+        batch_size=32,
+        shuffle=True, # 데이터를 섞을지 여부
+        seed=0,
+        subset='training', # train 인지 val인지 설정
+        rotation_range=30, # 회전제한 각도 30도
+        zoom_range=0.15, # 확대 축소 15%
+        width_shift_range=0.2, # 좌우이동 20%
+        height_shift_range=0.2, # 상하이동 20%
+        shear_range=0.15, # 반시계방햐의 각도
+        horizontal_flip=True, # 좌우 반전 True
+        fill_mode="nearest"
+        # 이미지 변경시 보완 방법 (constant, nearest, reflect, wrap) 4개 존재
+    )
+
+    val_images = train_generator.flow_from_dataframe(
+        dataframe=train_df,
+        x_col='Filepath',
+        y_col='Label',
+        target_size=(224, 224),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=32,
+        shuffle=True,
+        seed=0,
+        subset='validation',
+        rotation_range=30,
+        zoom_range=0.15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.15,
+        horizontal_flip=True,
+        fill_mode="nearest"
+    )
+
+    test_images = test_generator.flow_from_dataframe(
+        dataframe=test_df,
+        x_col='Filepath',
+        y_col='Label',
+        target_size=(224, 224),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=32,
+        shuffle=False
+    )
+    
+    return train_generator,test_generator,train_images,val_images,test_images
+
+models = {
+    "DenseNet121": {"model":tf.keras.applications.DenseNet121, "perf":0},
+    "MobileNetV2": {"model":tf.keras.applications.MobileNetV2, "perf":0},
+    "DenseNet201": {"model":tf.keras.applications.DenseNet201, "perf":0},
+    "EfficientNetB0": {"model":tf.keras.applications.EfficientNetB0, "perf":0},
+    "EfficientNetB1": {"model":tf.keras.applications.EfficientNetB1, "perf":0},
+    "InceptionV3": {"model":tf.keras.applications.InceptionV3, "perf":0},
+    "MobileNetV2": {"model":tf.keras.applications.MobileNetV2, "perf":0},
+    "MobileNetV3Large": {"model":tf.keras.applications.MobileNetV3Large, "perf":0},
+    "ResNet152V2": {"model":tf.keras.applications.ResNet152V2, "perf":0},
+    "ResNet50": {"model":tf.keras.applications.ResNet50, "perf":0},
+    "ResNet50V2": {"model":tf.keras.applications.ResNet50V2, "perf":0},
+    "VGG19": {"model":tf.keras.applications.VGG19, "perf":0},
+    "VGG16": {"model":tf.keras.applications.VGG16, "perf":0},
+    "Xception": {"model":tf.keras.applications.Xception, "perf":0}
+}
+# Create the generators
+train_generator,test_generator,train_images,val_images,test_images=create_gen()
+print('\n')
+
+def get_model(model):
+# Load the pretained model
+    kwargs =    {'input_shape':(224, 224, 3),
+                'include_top':False,
+                'weights':'imagenet',
+                'pooling':'avg'}
+    
+    pretrained_model = model(**kwargs)
+    pretrained_model.trainable = False # 레이어를 동결 시켜서 훈련중 손실을 최소화 한다.
+    
+    inputs = pretrained_model.input
+
+    x = tf.keras.layers.Dense(128, activation='relu')(pretrained_model.output)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+
+    outputs = tf.keras.layers.Dense(110, activation='softmax')(x)
+    # 라벨 개수가 8개이기 때문에 Dencs도 8로 설정
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    
+    return model
+'''
+# Train모델 학습
+for name, model in models.items():
+    
+    # 전이 학습 모델 가져오기
+    m = get_model(model['model'])
+    models[name]['model'] = m
+    
+    start = perf_counter()
+    
+    # 모델 학습
+    history = m.fit(train_images,validation_data=val_images,epochs=1,verbose=1)
+    
+    # 학습시간과 val_accuracy 저장
+    duration = perf_counter() - start
+    duration = round(duration,2)
+    models[name]['perf'] = duration
+    print(f"{name:20} trained in {duration} sec")
+    
+    val_acc = history.history['val_accuracy']
+    models[name]['val_acc'] = [round(v,4) for v in val_acc]
+
+# test데이터로 모델 성능 예측
+for name, model in models.items():
+    
+    # Predict the label of the test_images
+    pred = models[name]['model'].predict(test_images)
+    pred = np.argmax(pred,axis=1)
+
+    # Map the label
+    labels = (train_images.class_indices)
+    labels = dict((v,k) for k,v in labels.items())
+    pred = [labels[k] for k in pred]
+
+    y_test = list(test_df.Label)
+    acc = accuracy_score(y_test,pred)
+    models[name]['acc'] = round(acc,4)
+    print(f'**{name} has a {acc * 100:.2f}% accuracy on the test set**')
+   
+# Create a DataFrame with the results
+models_result = []
+
+for name, v in models.items():
+    models_result.append([ name, models[name]['val_acc'][-1], 
+                          models[name]['acc'],
+                          models[name]['perf']])
+    
+df_results = pd.DataFrame(models_result, 
+                          columns = ['model','val_accuracy','accuracy','Training time (sec)'])
+df_results.sort_values(by='accuracy', ascending=False, inplace=True)
+df_results.reset_index(inplace=True,drop=True)
+print(df_results)
+
+plt.figure(figsize = (15,5))
+sns.barplot(x = 'model', y = 'accuracy', data = df_results)
+plt.title('Accuracy on the test set (after 1 epoch))', fontsize = 15)
+plt.ylim(0,1)
+plt.xticks(rotation=90)
+plt.show()
+'''
+
+'''
+               model  val_accuracy  accuracy  Training time (sec)
+0        ResNet152V2        0.6985    0.6978               949.16
+1        DenseNet201        0.6909    0.6925               589.06
+2           Xception        0.6727    0.6747               512.34
+3         ResNet50V2        0.6744    0.6725               380.44
+4        InceptionV3        0.6508    0.6518               294.73
+5        DenseNet121        0.6368    0.6422               390.78
+6        MobileNetV2        0.5352    0.5255               185.85
+7              VGG16        0.4050    0.4045               750.45
+8              VGG19        0.3622    0.3536               899.69
+9   MobileNetV3Large        0.1909    0.1952               159.88
+10          ResNet50        0.1724    0.1711               444.91
+11    EfficientNetB1        0.0587    0.0600               370.49
+12    EfficientNetB0        0.0481    0.0517               273.30
+'''
+
+train_df,test_df = train_test_split(df, test_size=0.1, random_state=0)
+train_generator,test_generator,train_images,val_images,test_images=create_gen()
+
+model = get_model(tf.keras.applications.DenseNet201)
+history = model.fit(train_images,validation_data=val_images,epochs=7)
+
+model.save('./CIFAR_v2')
+
+pd.DataFrame(history.history)[['accuracy','val_accuracy']].plot()
+plt.title("Accuracy")
+plt.show()
+
+pd.DataFrame(history.history)[['loss','val_loss']].plot()
+plt.title("Loss")
+plt.show()
+
+# Predict the label of the test_images
+pred = model.predict(test_images)
+pred = np.argmax(pred,axis=1)
+
+# Map the label
+labels = (train_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+pred = [labels[k] for k in pred]
+
+def printmd(string):
+    # Print with Markdowns    
+    display(Markdown(string))
+    
+y_test = list(test_df.Label)
+acc = accuracy_score(y_test,pred)
+printmd(f'# Accuracy on the test set: {acc * 100:.2f}%')
+
+class_report = classification_report(y_test, pred, zero_division=1)
+print(class_report)
+
+
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+cf_matrix = confusion_matrix(y_test, pred, normalize='true')
+plt.figure(figsize = (10,7))
+sns.heatmap(cf_matrix, annot=False, xticklabels = sorted(set(y_test)), yticklabels = sorted(set(y_test)),cbar=False)
+plt.title('Normalized Confusion Matrix', fontsize = 23)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.show()
